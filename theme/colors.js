ac8e9dd0eefa473808459e97f19e5bc3375eaed2
Diego Costa <diegocosta.contato2021@gmail.com>
2025-05-30 15:36:27 -0300

    [Init] - Iniciando desenvolvimento

---
+++ theme/colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  background: '#fcfcfc',
+  black: '#323131',
+  white: '#FFFFFF',
+  gray: "#ececec",
+  aqua: '#00FFFF',
+  red: '#E54A4A',
+  warn: '#E5AF4A',
+  grayDark: '#d3d2d2',
+};
+
+export default Colors;
